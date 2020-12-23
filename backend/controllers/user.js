48eafa8245472aeaa85991f9bb35b27f813f4daf
Artur01300 <artur.karapetyan@outlook.fr>
2020-12-23 17:26:54 +0100

    Création d'utilisateurs: login configuré

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,20 +1,58 @@
 //on a besoin du paquage du cryptage pour les mode passe (--save bcrypt)
 const bcrypt = require('bcrypt');
 
-//Ce controleur a besoin 2 midlewar: la function signup et login
-exports.signup = (req, res, next) => {//Permet d'enrégister de ouveau users
-    bcrypt.hash(req.body.password, 10)
-    .then(hash => {
-        const user = new User({
-            email: req.body.email,
-            password: hash
-        });
-        user.save()
-        .then(() => res.status(201).json({message: 'Utilisatur créé !'}))
-        .catch(error => res.status(400).json({error}));
-    })
-    .catch(error => res.status(400).json({error}));
-}
-exports.login = (req, res, next) => {//Permet de connécter les users existant
+//on a besoin  de notre model user car on va enrégister et lire des user dans ce middlwar
+const User = require('../models/User');
 
+//Ce contrôleur a besoin 2 middlwars: la function signup et login
+//on hache le mode de passe avec une function(bcrypt.hash()) ansachon 
+//avec la hache créé par bcrypt on va ensuite enregistrer l'user dans la base de données
+//on lui passe le password du core de la requette qui serra passer par le frontend
+//le solde(10) combien de l'algoritme de hachage c'est à dire 10 tour qui est suffisent de créer un mot de passe sécurisé
+//le .then récupère le hache de mot de passe et en suit on vas engrégistrer dans un nouveau user(const user = new User) dans la base de donnée
+exports.signup = (req, res, next) => {//Permet d'enrégister de nouveau users(req.body.password)
+    bcrypt.hash(req.body.password, 10)
+    .then(hach => {//on récupère le hache de mot de passe et en suit on enrégistre dans nouveau user(new User) et ensuit on onrégistre dans la basse de donné
+        const user = new User({//créatio de notre nouvelle outilisateur avec le modèle mongoose
+            email: req.body.email,//en passe l'adresse quie fournie dans le core de la requette
+            password: hach//on enrégisre le hache qui est crée 2 ligne à haut(.then(hach =>{)
+        });
+        user.save()//On enrégistre dans la base de donnée
+        .then(()=>res.status(201).json({message: 'Utilisateur créé !'}))//en envoie 201 pour la création de reçource et renvoie le message
+        .catch(error => res.status(400).json({error}))
+    })
+    .catch(error => res.status(500).json({error}));//error server(500) qu'on voie dans un objet
+}
+
+//Login permet de connecter aux users exictent 
+//En premier temps on va commencer de trouver les utilisateurs dans la base de données qui correspond à l'adresse email entré par lutilisateur de l'application 
+//et si jamais l'utilisateur n'existe pas en envoir une erreur
+//La méthode findeOne permet de trouver un  seul utilisateur dans la base de données et que l'adresse me il est unique et on compart ({email: req.body.email})
+//on veut que ce soit l'utilisateur pour qui l'adresse mail correspond à l'adresse mail envoyée dans la requête
+//findOne est un function ansycron
+//on va utiliser le bcrypt pour comparer le mot de passe envoyé par l'utilisateur avec l'hach qui est enregistré avec l'user qu'ont ici (.then(user=> if...))
+//on utilise la fonction compart(bcrypt.copare) pour comparer. on compart le mot de passe envoyé avec la requête (req.body.password,) et avec le hach (user.password)
+//qui est enregistré dans notre user, dans notre document user
+exports.login = (req, res, next) => {//Permet de connécter les users existant 
+    User.findOne({email: req.body.email})//
+    .then(user => {
+        if(!user){//si on n'a pas trouver les user on envoie le 401 pour dire non autorisé
+            return res.status(401).json({error: 'Utilisateur non trouvé !'});
+        }
+        //si user est bon alors on cript le mot de passe
+        bcrypt.compare(req.body.password, user.password)
+        .then(valid =>{
+            if(!valid){//il reçoit le boolean, c'est-à-dire, si le mot de passe n'est pas valable
+                return res.status(401).json({error: 'Mot de passe incorrect !'});
+            }
+            //si on arrive ici alors la comparaison est true. dans ce cas-là on renvoie la bonne connexion et l'objet json qui contient id d'user dans la base
+            //et on envoe la token
+            res.status(200).json({
+                userId: user._id,
+                token: 'TOKEN'
+            });
+        })
+        .catch(error => res.status(500).json({error}));
+    })
+    .catch(error => res.status(500).json({error}));
 }
