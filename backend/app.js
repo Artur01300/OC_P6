ab2118c85bb20314f8a232f10695020424d55551
Artur01300 <artur.karapetyan@outlook.fr>
2020-12-23 12:29:04 +0100

    Fchier app.js créé

---
+++ backend/app.js
@@ -0,0 +1,5 @@
+const express = require('express');//Importation d'express
+
+const app = express();//Il serra notre appliquation
+
+module.exports = app;//On éxporte cet appliquation pour qu'on puisse accedès depuis un autre fichier dans notre projet
