50239587a54757eeae4c8745ca45297d47fa04e7
Artur01300 <artur.karapetyan@outlook.fr>
2020-12-25 13:09:32 +0100

    Modifications apportés models/Sauce.js: schèma modifié, route monufacturer ajouté

--- backend/models/Sauce.js
+++ backend/models/Sauce.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');//Importation du mongoose
 
 //Création du Schema des données 'thingSchema' en utilisant la fonction mongoose dans le quelle on vas passer un onbjet 
 //qui vas dicter les différant chemps dant notre thing aurra besoin
-const thingSchema = mongoose.Schema({
-    // id: {type: Object, required: true},
+const saucesSchema = mongoose.Schema({
     userId: {type: String, required: true},//La clé du nome de cheme est 'userId' et on crée un objet pour confiqurer le id
     name: {type: String, required: true},//required ture signifie sans name on ne pourra pas enrégistrer un thing dans la base
+    manufacturer: {type: String, required: true},
     description: {type: String, required: true},
     mainPepper: {type: String, required: true},
     imageUrl: {type: String, required: true},
@@ -19,4 +19,4 @@ const thingSchema = mongoose.Schema({
 //Nous exportons ce schéma en tant que modèle Mongoose appelé « Thing », le rendant par là même disponible pour notre application Express.
 //Exprotation le modéle terminé(module.exports) et on export le mongouse.model.
 //Le premier argument passé c'est le nom du type('Thing'), douzième argoument c'est le schéma qu'on va utiliser(thingSchema), qu'on a créé
-module.exports = mongoose.model('Sauce', thingSchema);
+module.exports = mongoose.model('Sauce', saucesSchema);
